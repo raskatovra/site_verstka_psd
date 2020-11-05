3b596c87d68dfe759606378e70e9e573e859d01a
raskatovra <70175597+raskatovra@users.noreply.github.com>
2020-11-05 22:12:10 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+$(function () {
+		
+	
+	
+	$('.sliderinner').slick({
+		nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+		prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+		infinite: false
+	});
+
+});
